3475b4edc86fe851d06b0de5f1108b357ae3b693
Morella92 <morena.piem@outlook.com>
2023-02-28 16:38:41 +0100

    FEAT bonus

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,32 +1,26 @@
 <script>
 //importo il componente
 import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
 
 export default{
   components: {
     //registro il componente
-    AppTitle
+    AppTitle,
+    AppHero
   },
 }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <div>
-    <h1>
-      {{ title }}
-    </h1>
+
+  <div class="bg-image">
+    
   </div>
 
   <!-- utilizzo il componente e stampo il titolo -->
-  <AppTitle></AppTitle>
+  <!-- <AppTitle></AppTitle> -->
+  <AppHero></AppHero>
 </template>
 
 <style scoped>
@@ -42,4 +36,5 @@ export default{
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+
 </style>
---
+++ my-vue-app/src/components/AppHero.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="bg-image">
+        <h1 class="bg-green">
+            {{ title }}
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+            title: 'La mia prima app con Vite!',
+
+        }
+    }
+}
+</script>
+
+<style>
+.bg-image {
+    background-image: url(../img/images.jpg);
+
+}
+
+.bg-green {
+    background-color: green;
+    color: white;
+    font-weight: 600;
+}
+</style>
